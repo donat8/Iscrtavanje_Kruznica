09ae1348f1990cb00da022313ebe96c3cc5b9e9b
Dona Pejnović <don@DESKTOP-QROSDFJ>
2020-02-12 06:39:36 +0100

    Add project files.

---
+++ kruznica2/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace kruznica2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DeepPink;
+            this.label1.Location = new System.Drawing.Point(0, 217);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(602, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Radius je: ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.ClientSize = new System.Drawing.Size(602, 243);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Name = "Form1";
+            this.Text = "Kruznica";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ kruznica2/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace kruznica2
+{
+   
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+           //SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.Opaque | ControlStyles.OptimizedDoubleBuffer | ControlStyles.ResizeRedraw | ControlStyles.UserPaint, true);
+        }
+        class Kruznica
+        {
+            public Point pocetak, kraj;
+            public double radius;
+            public Rectangle rect;
+            public double IzrRadius(Point poc, Point kraj)
+            {
+                int x1 = Math.Abs(poc.X - kraj.X);
+                int y1 = Math.Abs(poc.Y - kraj.Y);
+
+                radius = Math.Sqrt((x1 * x1) + (y1 * y1));
+                return radius;
+            }
+            public void NacrtajKruznicu(Graphics g)
+            {
+                g.DrawLine(Pens.Black, pocetak, kraj);
+                using (Pen pinky = new Pen(Color.DeepPink, 4F))
+                    g.DrawEllipse(pinky, rect);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        List<Kruznica> kruznice =new List<Kruznica>();
+     
+        class StvaranjeKruznice:Kruznica
+        {        
+            public Kruznica Kruznica;
+        }
+        StvaranjeKruznice stvaranjeKruznice;  //tu se spremaju kruznice u stvaranju       
+       
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Invalidate(); //briše formu i opet ju crta dok se prozor povećava/smanjuje
+        }
+              
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            
+            if (e.Button == MouseButtons.Left) {
+                Capture = true;
+                stvaranjeKruznice = new StvaranjeKruznice
+                {
+                    pocetak = e.Location,
+                    Kruznica = new Kruznica { }
+                };
+                stvaranjeKruznice.Kruznica.pocetak = e.Location;
+               
+            }
+            base.OnMouseDown(e); 
+        }
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            if (Capture) { 
+                NovoIscrtavanjeKruznice(e.Location);
+                stvaranjeKruznice.Kruznica.kraj = e.Location;
+            }
+            base.OnMouseMove(e);
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            if (Capture && e.Button == MouseButtons.Left) {
+                Capture = false;
+                NovoIscrtavanjeKruznice(e.Location);
+               if (stvaranjeKruznice.Kruznica.kraj.X > 0 && stvaranjeKruznice.Kruznica.kraj.Y > 0)
+                    kruznice.Add(stvaranjeKruznice.Kruznica);
+                stvaranjeKruznice = null;
+               // Invalidate();        //dok se miš pusti da se nacrta
+            }
+            base.OnMouseUp(e);  //
+        }
+
+        private void NovoIscrtavanjeKruznice(Point kraj)
+        {     
+            Point pocetak = stvaranjeKruznice.pocetak;
+            
+            int Radius = (int)stvaranjeKruznice.IzrRadius(pocetak, kraj);
+
+            stvaranjeKruznice.Kruznica.rect
+                = new Rectangle(pocetak.X - Radius, pocetak.Y - Radius,Radius * 2, Radius * 2);
+
+            Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            foreach (Kruznica kruznica in kruznice)
+            {           
+                g.DrawLine(Pens.Black, kruznica.pocetak, kruznica.kraj);
+                kruznica.NacrtajKruznicu(g);
+                kruznica.IzrRadius(kruznica.pocetak, kruznica.kraj);
+                label1.Invalidate();
+                label1.Text = $"radius je: {(int)kruznica.radius}";              
+            }
+           
+            if (stvaranjeKruznice != null)
+                stvaranjeKruznice.Kruznica.NacrtajKruznicu(g);
+
+            base.OnPaint(e);
+            g.Dispose();
+        }
+
+    }
+}
